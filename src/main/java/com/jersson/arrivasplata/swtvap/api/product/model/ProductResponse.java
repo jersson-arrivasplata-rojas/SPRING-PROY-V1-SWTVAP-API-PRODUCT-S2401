431880b665c8858f0763d3b7e247ebfea1320827
Jersson Giomar Arrivasplata Rojas <jgrojas@stefanini.com>
2024-02-11 15:09:58 -0500

    Add files2

--- src/main/java/com/jersson/arrivasplata/swtvap/api/product/model/ProductResponse.java
+++ src/main/java/com/jersson/arrivasplata/swtvap/api/product/model/ProductResponse.java
@@ -1,9 +1,10 @@
 package com.jersson.arrivasplata.swtvap.api.product.model;
 
-import com.jersson.arrivasplata.swtvap.api.product.enums.Status;
-import lombok.Data;
-
 import java.math.BigDecimal;
+
+import com.jersson.arrivasplata.swtvap.api.product.enums.Status;
+
+import lombok.Data;
 
 @Data
 public class ProductResponse {
@@ -12,6 +13,8 @@ public class ProductResponse {
     private String name;
     private String description;
     private BigDecimal price;
+    private BigDecimal priceUSD;
+    private BigDecimal priceEUR;
     private Long stock;
     private Long stockMin;
     private String otherDetails;
